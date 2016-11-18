74b8e6b8e3ae0cb0033523fa036a7d6045ae7409
nipil <nipil@users.noreply.github.com>
2016-11-18 12:33:27 +0100

    add delegation trace

--- src/main/groovy/EmailScriptSpec.groovy
+++ src/main/groovy/EmailScriptSpec.groovy
@@ -1,8 +1,10 @@
 abstract class EmailScriptSpec extends Script {
     def email(Closure cl) {
+        println "EmailScriptSpec: email keywork closure delegation start"
         def email = new EmailSpec()
         def code = cl.rehydrate(email, this, this)
         code.resolveStrategy = Closure.DELEGATE_ONLY
         code()
+        println "EmailScriptSpec: email keywork closure delegation ends"
     }
 }
--- src/main/groovy/EmailSpec.groovy
+++ src/main/groovy/EmailSpec.groovy
@@ -6,9 +6,11 @@ class EmailSpec {
     void subject(String subject) { println "Subject: $subject" }
 
     void body(Closure body) {
+        println "EmailSpec: email keywork closure delegation start"
         def bodySpec = new BodySpec()
         def code = body.rehydrate(bodySpec, this, this)
         code.resolveStrategy = Closure.DELEGATE_ONLY
         code()
+        println "EmailSpec: email keywork closure delegation ends"
     }
 }
